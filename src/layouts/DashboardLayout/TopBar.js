55553676b7a9892d3c9f89da98e4a5df70b710d0
agesa3 <collinsagesa2000@gmail.com>
2021-07-19 01:52:35 +0300

    more update on navbar

--- src/layouts/DashboardLayout/TopBar.js
+++ src/layouts/DashboardLayout/TopBar.js
@@ -1,81 +1,308 @@
-import React, { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import {
-  AppBar,
-  Badge,
-  Box,
-  Hidden,
-  IconButton,
-  Toolbar,
-  makeStyles
-} from '@material-ui/core';
+// import React, { useState } from 'react';
+// import { Link as RouterLink } from 'react-router-dom';
+// import clsx from 'clsx';
+// import PropTypes from 'prop-types';
+// import {
+//   AppBar,
+//   Badge,
+//   Box,
+//   Hidden,
+//   IconButton,
+//   Toolbar,
+//   makeStyles
+// } from '@material-ui/core';
+// import Typography from '@material-ui/core/Typography';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import { Button, Grid } from '@material-ui/core';
+// import 'src/layouts/DashboardLayout/style.css'
+
+// const useStyles = makeStyles(theme => ({
+//   root: {},
+//   avatar: {
+//     width: 60,
+//     height: 60
+//   },
+//   title: {
+//     color: theme.palette.common.white
+//   },
+//   buttonTest: {
+//     color: theme.palette.common.white
+//   },
+
+// }));
+
+// const TopBar = ({ className, onMobileNavOpen, ...rest }) => {
+//   const classes = useStyles();
+//   const [notifications] = useState([]);
+
+//   return (
+//     <AppBar className={clsx(classes.root, className)} elevation={0} {...rest}>
+//       <Toolbar>
+//         <RouterLink to="/dashboard">
+//           <Typography variant="h6" className={classes.title} noWrap>
+//             Exploring and Visualizing Healthcare Data
+//           </Typography>
+//         </RouterLink>
+//         <Box flexGrow={1} />
+//         <Hidden mdDown>
+//           <IconButton color="inherit">
+//             <Badge
+//               badgeContent={notifications.length}
+//               color="primary"
+//               variant="dot"
+//             ></Badge>
+//           </IconButton>
+//           <IconButton color="inherit"></IconButton>
+//         </Hidden>
+//         <Hidden lgUp>
+//           <IconButton color="inherit" onClick={onMobileNavOpen}>
+//             <MenuIcon />
+//           </IconButton>
+//         </Hidden>
+//       </Toolbar>
+//     </AppBar>
+//   );
+// };
+
+// TopBar.propTypes = {
+//   className: PropTypes.string,
+//   onMobileNavOpen: PropTypes.func
+// };
+
+// export default TopBar;
+import React from 'react';
+import {  makeStyles } from '@material-ui/core/styles';
+import { alpha } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {},
-  avatar: {
-    width: 60,
-    height: 60
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
   },
   title: {
-    color: theme.palette.common.white
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch'
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
   }
 }));
 
-const TopBar = ({
-  className,
-  onMobileNavOpen,
-  ...rest
-}) => {
+export default function TopBar() {
   const classes = useStyles();
-  const [notifications] = useState([]);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = event => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <AppBar
-      className={clsx(classes.root, className)}
-      elevation={0}
-      {...rest}
-    >
-      <Toolbar>
-        <RouterLink to="/">
-        <Typography variant="h6" className={classes.title} noWrap>
-          Exploring and Visualizing Healthcare Data
-        </Typography>
-        </RouterLink>
-        <Box flexGrow={1} />
-        <Hidden mdDown>
-          <IconButton color="inherit">
-            <Badge
-              badgeContent={notifications.length}
-              color="primary"
-              variant="dot"
-            >
-            </Badge>
-          </IconButton>
-          <IconButton color="inherit">
-          </IconButton>
-        </Hidden>
-        <Hidden lgUp>
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
           <IconButton
+            edge="start"
+            className={classes.menuButton}
             color="inherit"
-            onClick={onMobileNavOpen}
+            aria-label="open drawer"
           >
             <MenuIcon />
           </IconButton>
-        </Hidden>
-      </Toolbar>
-    </AppBar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Material-UI
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
   );
-};
+}
 
-TopBar.propTypes = {
-  className: PropTypes.string,
-  onMobileNavOpen: PropTypes.func
-};
 
-export default TopBar;
+
