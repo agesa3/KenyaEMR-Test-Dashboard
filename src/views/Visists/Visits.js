fd7a7d10accf89670e99652d8230c7286f9a5083
agesa3 <collinsagesa2000@gmail.com>
2021-08-12 02:37:48 +0300

    update on conflicts

--- src/views/Visists/Visits.js
+++ src/views/Visists/Visits.js
@@ -1,34 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from 'axios'
 
 const Visits = () => {
+
   const [visits, setVisits] = useState([]);
+
   useEffect(() => {
-    axios
-      .get('visits.json')
-      .then(res => {
-        console.log(res.data.results);
-        const n = res.data.results;
-        console.log(n.length);
-        setVisits(res.data.results);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+    axios .get("visits.json")
+    .then((res) => {
+      console.log(res.data.results);
+      const n = res.data.results;
+      console.log(n.length);
+      setVisits(res.data.results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  }, [])
 
   return (
     <div>
-      {visits.map(visit => (
-        <p key={visit.uuid}>
-          <ul>
-            <li>{visit.patient.display}</li>
-            <li>{visit.visitType.display}</li>
-          </ul>
-        </p>
-      ))}
+      {visits.map(visit=>(
+          <p key={visit.uuid}>{visit.patient.display}</p>
+        )
+      )}
     </div>
-  );
+  )
 };
 
 export default Visits;
